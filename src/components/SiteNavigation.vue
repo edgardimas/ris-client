2557c8f023d3270944b35b4eeadc8908b5871057
Edgar Dimas <edgar.dimas.ir@gmail.com>
2023-07-06 11:44:58 +0700

    navbar feat.

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,11 +1,22 @@
 <template>
-  <header class="sticky top-0 bg-weather-primary shadow-lg">
-    <nav class="container flex flex-col sm:flex-row items-senter gap-4 text-white py-6">
-      <div class="flex items-center gap-3 flex-1">
-        <i class="fa-solid fa-sun text-lg"></i>
+  <header class="sticky top-0 bg-theme-primary shadow-lg">
+    <nav class="flex sm:flex-row items-center gap-6 justify-between text-white">
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex gap-4 bg-theme-secondary py-6 px-8 items-center">
+          <img src="../assets/icons/mainLogo.png" class="w-10" />
+          <p class="text-2xl">TawadaRIS</p>
+        </div>
+      </RouterLink>
+
+      <div class="flex justify-end flex-row gap-10 px-28">
+        <div class="hover:text-green-100">Home</div>
+        <div class="hover:text-green-100">About</div>
+        <div class="hover:text-green-100">User</div>
       </div>
     </nav>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: { 'weather-primary': '#00668A', 'weather-secondary': '#004E71' }
+      colors: {
+        'theme-primary': '#49726F',
+        'theme-secondary': '#436664',
+        'theme-background': '#424242'
+      }
+    },
+    fontFamily: {
+      Roboto: ['Roboto, sans-serif']
+    },
+    screens: {
+      sm: '640px',
+      md: '768px'
     }
-  },
-  fontFamily: {
-    Roboto: ['Roboo, sans-serif']
-  },
-  container: {
-    padding: '2rem',
-    center: true
-  },
-  screens: {
-    sm: '640px',
-    md: '768px'
   },
   plugins: []
 }
