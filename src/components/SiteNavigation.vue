a259dcc5266b7e99629aceeede7bc12467633fa1
Edgar Dimas <edgar.dimas.ir@gmail.com>
2023-07-06 09:59:10 +0700

    init and vue conf.

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav class="container flex flex-col sm:flex-row items-senter gap-4 text-white py-6">
+      <div class="flex items-center gap-3 flex-1">
+        <i class="fa-solid fa-sun text-lg"></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup></script>
