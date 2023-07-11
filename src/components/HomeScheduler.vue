bd29fc81b09a0592ea3b5f04f69c68cfbc25ff85
Edgar Dimas <edgar.dimas.ir@gmail.com>
2023-07-11 16:02:37 +0700

    added scheduler feat.

--- src/components/HomeScheduler.vue
+++ src/components/HomeScheduler.vue
@@ -23,11 +23,84 @@ const form = ref({
 })
 </script>
 
+<script>
+import { DayPilot, DayPilotCalendar } from '@daypilot/daypilot-lite-vue'
+import { onMounted } from 'vue'
+
+export default {
+  name: 'WeeklyCalendar',
+  props: {},
+  components: {
+    DayPilotCalendar
+  },
+  setup() {
+    const calendarRef = ref(null)
+
+    const config = {
+      viewType: 'Week',
+      startDate: DayPilot.Date.today(),
+      onTimeRangeSelected: (args) => {
+        DayPilot.Modal.prompt('Create a new event:', 'Event 1').then((modal) => {
+          var dp = args.control
+          dp.clearSelection()
+          if (modal.canceled) {
+            return
+          }
+          dp.events.add({
+            start: args.start,
+            end: args.end,
+            id: DayPilot.guid(),
+            text: modal.result
+          })
+        })
+      },
+      eventDeleteHandling: 'Disabled',
+      onEventMoved: () => {
+        console.log('Event moved')
+      },
+      onEventResized: () => {
+        console.log('Event resized')
+      }
+    }
+
+    const loadEvents = () => {
+      // placeholder for an HTTP call
+      var data = [
+        {
+          id: 1,
+          start: DayPilot.Date.today().addHours(10),
+          end: DayPilot.Date.today().addHours(11),
+          text: 'Event 1'
+        },
+        {
+          id: 2,
+          start: DayPilot.Date.today().addHours(13),
+          end: DayPilot.Date.today().addHours(16),
+          text: 'Event 2'
+        }
+      ]
+      calendarRef.value.control.update({ events: data })
+    }
+
+    onMounted(() => {
+      loadEvents()
+      console.log('Mounted.')
+    })
+
+    return {
+      config,
+      calendarRef
+    }
+  }
+}
+</script>
+
 <template>
   <div class="p-4 max-w-sm">
     <form action="#" class="flex flex-col space-y-6">
       <DropDown placeholder="Select Department" v-model="form.person_id" :options="people" />
       <DropDown placeholder="Select Modalities" v-model="form.role_ids" :options="roles" multiple />
     </form>
+    <DayPilotCalendar id="dp" :config="config" ref="calendarRef" />
   </div>
 </template>
